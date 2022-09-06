cf3e9b50a2f883d108c18159ac6f65c5d87a1db7
haider ali <ha347278@gmail.com>
2022-09-06 18:08:15 +0500

    inflator

--- app/src/main/java/com/example/recyclerview/myadapter.java
+++ app/src/main/java/com/example/recyclerview/myadapter.java
@@ -1,5 +1,6 @@
 package com.example.recyclerview;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -19,7 +20,10 @@ String data[];
     @NonNull
     @Override
     public holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.singlerow,parent,false);
+        return new holder(view);
     }
 
     @Override
