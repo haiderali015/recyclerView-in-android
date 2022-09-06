ab19e195fe7ac76c2c72ad85003e82839fc3aa91
haider ali <ha347278@gmail.com>
2022-09-06 17:58:03 +0500

    class set

--- app/src/main/java/com/example/recyclerview/myadapter.java
+++ app/src/main/java/com/example/recyclerview/myadapter.java
@@ -1,7 +1,19 @@
 package com.example.recyclerview;
 
+import android.view.View;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class myadapter extends RecyclerView.Adapter {
+
+
+    class holder extends RecyclerView.ViewHolder{
+        
+
+        public holder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 
 }
