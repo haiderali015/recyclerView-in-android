15575a8019d32aca4cb2dad7d136679429affea9
haider ali <ha347278@gmail.com>
2022-09-06 18:00:08 +0500

    att set

--- app/src/main/java/com/example/recyclerview/myadapter.java
+++ app/src/main/java/com/example/recyclerview/myadapter.java
@@ -1,6 +1,8 @@
 package com.example.recyclerview;
 
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -9,10 +11,15 @@ public class myadapter extends RecyclerView.Adapter {
 
 
     class holder extends RecyclerView.ViewHolder{
-        
+        ImageView img;
+        TextView txt;
+
 
         public holder(@NonNull View itemView) {
             super(itemView);
+            img = (ImageView) itemView.findViewById(R.id.img1);
+            txt=(TextView) itemView.findViewById(R.id.t1);
+            
         }
     }
 
