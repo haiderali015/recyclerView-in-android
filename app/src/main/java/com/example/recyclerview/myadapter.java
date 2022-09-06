ade86e6e076855ffc720830f5b73ae64c065ec21
haider ali <ha347278@gmail.com>
2022-09-06 18:01:00 +0500

    imp methods

--- app/src/main/java/com/example/recyclerview/myadapter.java
+++ app/src/main/java/com/example/recyclerview/myadapter.java
@@ -1,6 +1,7 @@
 package com.example.recyclerview;
 
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -9,6 +10,22 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class myadapter extends RecyclerView.Adapter {
 
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 
     class holder extends RecyclerView.ViewHolder{
         ImageView img;
@@ -19,7 +36,7 @@ public class myadapter extends RecyclerView.Adapter {
             super(itemView);
             img = (ImageView) itemView.findViewById(R.id.img1);
             txt=(TextView) itemView.findViewById(R.id.t1);
-            
+
         }
     }
 
